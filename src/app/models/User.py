51016f2d9097be325343d73eef3a81217f588794
seonwoo0808 <seonwoo0808@naver.com>
2022-08-12 11:38:12 +0900

    update user model

--- src/app/models/User.py
+++ src/app/models/User.py
@@ -6,6 +6,11 @@ class User(db.Model):
     user_email = db.Column(db.String(120), unique=True, nullable=False)
     user_pw = db.Column(db.String(500), nullable=False)
     created = db.Column(db.DateTime, nullable=False)
+    user_age = db.Column(db.Integer, nullable=False)
+    user_job_status = db.Column(db.String(20), nullable=False)
+    user_academic_status = db.Column(db.String(20), nullable=False)
+    user_specialization = db.Column(db.String(100), nullable=False)
+    user_pre_startup = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.email
