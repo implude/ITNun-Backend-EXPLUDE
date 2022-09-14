5db3cff555cf367f2ee6b846c6cf1abbddf3d515
PENTAGON <seonwoo0808@naver.com>
2022-09-14 21:12:42 +0900

    add get all policy func

--- src/app/views/policy_manage_views.py
+++ src/app/views/policy_manage_views.py
@@ -8,40 +8,6 @@ import os, datetime
 
 bp = Blueprint('policy', __name__, url_prefix='/policy')
 
-
-@bp.route('/update_policy', methods=['POST'])
-def update_policy():
-    if request.is_json:
-        try:
-            params = request.get_json()
-            request_key = params['requset_key']
-        except:
-            return jsonify({'result': 'fail', 'message': 'invalid params'})
-        if request_key == os.environ['REQUEST_KEY']:
-            policy_data = open_api.get_youth_policy()
-            Policy.query.delete()
-            object_list = []
-            for i in policy_data:
-                object_list.append(Policy(policy_id=i['bizId'], 
-                                policy_name=i['polyBizSjnm'], 
-                                policy_Biz_code=i['polyBizSecd'],
-                                policy_type=i['plcyTpNm'], 
-                                policy_description=i['polyItcnCn'], 
-                                policy_spor_amount=i['sporScvl'], 
-                                policy_spor_description=i['sporCn'], 
-                                policy_age=i['ageInfo'], 
-                                policy_job_status=i['empmSttsCn'], 
-                                policy_academic_status=i['accrRqisCn'], 
-                                policy_specialization=i['majrRqisCn'],
-                                policy_good_at=i['splzRlmRqisCn'],
-                                policy_request_deadline=i['rqutPrdCn'],
-                                policy_website_url=i['rqutUrla']))
-                db.session.add(object_list[-1])
-            db.session.commit()
-                
-            return jsonify({'result': 'success'})
-        return jsonify({'result': 'fail', 'message': 'invalid request key'})
-
 @bp.route('/get_policy', methods=['POST'])
 def get_policy():
     if request.is_json:
@@ -94,6 +60,26 @@ def get_policy():
             db.session.add_all(not_read_policy_objetct)
             db.session.commit()
             return jsonify({"policy": not_read_policy_list})
+        else:
+            return jsonify({'result': 'fail', 'message': token_auth_info[1]})
+    else:
+        return jsonify({"result": "invalid request"})
+
+@bp.route('/get_all_policy', methods=['POST'])
+def get_policy():
+    if request.is_json:
+        params = request.get_json()
+        try:
+            token = params['token']
+        except:
+            return jsonify({'result': 'fail', 'message': 'token not found'})
+        token_auth_info = token_auth.token_decode(token)
+        if token_auth_info[0]:
+            policy_dict_list = []
+            policy_object = Policy.query.all()
+            for i in policy_object:
+                policy_dict_list.append(i.as_dict())
+            return jsonify({"policy": policy_dict_list})
         else:
             return jsonify({'result': 'fail', 'message': token_auth_info[1]})
     else:
