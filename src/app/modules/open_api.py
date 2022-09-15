22976a7fa242af0b5de5eaf27d6fa1b89abf036f
PENTAGON <seonwoo0808@naver.com>
2022-09-15 20:30:49 +0900

    bug fix2

--- src/app/modules/open_api.py
+++ src/app/modules/open_api.py
@@ -23,7 +23,7 @@ def get_youth_policy():
 
     list_type = []
     url = "https://www.youthcenter.go.kr/opi/empList.do"
-    api_key = os.environ.get('YOUTH_POLICY_API_KEY')
+    api_key = os.environ.get('YOUTH_API_KEY')
 
     for i in range(1, iteration+2):
         params = {'openApiVlak': api_key, 'pageIndex': i, 'display': 100}
@@ -56,7 +56,7 @@ def get_youth_space():
 
     list_type = []
     url = "https://www.youthcenter.go.kr/opi/wantedSpace.do"
-    api_key = os.environ.get('YOUTH_SPACE_API_KEY')
+    api_key = os.environ.get('YOUTH_API_KEY')
 
     for i in range(1, iteration+2):
         params = {'openApiVlak': api_key, 'pageIndex': i, 'display': 100, 'pageType': 1}
