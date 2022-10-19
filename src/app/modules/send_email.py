98f3dfa5487efc72ce2b5306a100c430fdc3e331
PENTAGON <seonwoo0808@naver.com>
2022-10-19 21:43:45 +0900

    add verification mail sending func

---
+++ src/app/modules/send_email.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv   
+load_dotenv() #load .env fi
+import smtplib, os
+from email.mime.text import MIMEText
+
+GOOGLE_APP_PASSWORD = os.environ["GOOGLE_APP_PASSWORD"]
+EMAIL_ADDRESS = os.environ["EMAIL_ADDRESS"]
+
+def send_verification_email(email, email_verification_url):
+
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+
+    smtp.ehlo()
+
+    smtp.starttls()
+
+    smtp.login(EMAIL_ADDRESS, GOOGLE_APP_PASSWORD)
+
+    msg = MIMEText(email_verification_url)
+    msg['Subject'] = '잇는 | 이메일 인증'
+
+    smtp.sendmail(EMAIL_ADDRESS, email, msg.as_string())
+
+    smtp.quit()
